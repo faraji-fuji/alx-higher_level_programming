56c36f7918115fff536b88dbbe9015cfa4363d60
faraji-fuji <farajifuji@gmail.com>
2022-06-04 08:50:27 +0000

    calculation, correction

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import add from calculator_1
-    import sub from calculator_1
-    import mul from calculator_1
-    import div from calculator_1
+    from calculator_1 import add
+    from calculator_1 import sub
+    from calculator_1 import mul
+    from calculator_1 import div
     a = 10
     b = 5
     print("{} + {} = {}".format(a, b, add(a, b)))
