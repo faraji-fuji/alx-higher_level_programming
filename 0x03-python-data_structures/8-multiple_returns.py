58a796bba1eb4a32e0817e490ad4334f3cd835b6
faraji-fuji <farajifuji@gmail.com>
2022-06-05 10:52:20 +0000

    multiple returns, correction

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
+    if sentence == "":
+        t = (0, None)
+        return t
     first_char = sentence[0]
     length = len(sentence)
-    if sentence == "":
-        first_char = None
     t = (length, first_char)
     return t
