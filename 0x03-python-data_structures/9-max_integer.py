873ffea1a6fc3a4bfeae49fe9553c53590bbd655
faraji-fuji <farajifuji@gmail.com>
2022-06-05 08:44:33 +0000

    max integer

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    for i in range(len(my_list) - 1):
+        if my_list[i] > my_list[i + 1]:
+            temp = my_list[i]
+            my_list[i] = my_list[i + 1]
+            my_list[i + 1] = temp
+    return my_list[-1]
