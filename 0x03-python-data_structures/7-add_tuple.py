b1311016331d6d8a04a0924aa7dc79e8def2b229
faraji-fuji <farajifuji@gmail.com>
2022-06-05 10:56:01 +0000

    add tuple, correction

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,17 +3,17 @@ def add_tuple(tuple_a=(), tuple_b=()):
     new_tuple_a = tuple_a[:]
     new_tuple_b = tuple_b[:]
     if len(tuple_a) > 2:
-        new_tuple_a = tuple_a[:3]
+        new_tuple_a = tuple_a[:2]
     if len(tuple_a) == 1:
         new_tuple_a = tuple_a + (0,)
     if len(tuple_a) == 0:
-        new_tuple_a = tuple_a + (0,0)
+        new_tuple_a = tuple_a + (0, 0)
     if len(tuple_b) > 2:
-        new_tuple_b = tuple_b[:3]
+        new_tuple_b = tuple_b[:2]
     if len(tuple_b) == 1:
         new_tuple_b = tuple_b + (0,)
     if len(tuple_b) == 0:
-        new_tuple_b = tuple_b + (0,0)
+        new_tuple_b = tuple_b + (0, 0)
     a, b = new_tuple_a
     c, d = new_tuple_b
     e = a + c
